59e3ee6c648e0b753f7840b795449d49ade80feb
jstevenson2293 <jstevenson2293@gmail.com>
2023-05-06 17:01:22 -0400

    vowel remover

--- 8kyu/Vowel Remover/main.js
+++
@@ -1,9 +0,0 @@
-
-//Code Wars challange. Remove Vowels from a string.
-
-function shortcut (string) {
-  
-  let vowels = ['a', 'e', 'i', 'o', 'u']
-  return string.split('').filter(item =>!vowels.includes(item)).join('')
-  ;
-}
